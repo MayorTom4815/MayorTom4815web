f508551106a37b9beaf5b8c678d09523841d6373
MayorTom4815 <domiisac2004@proton.me>
2025-10-09 03:03:06 -0600

    refacto 9Oct2025

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-var emailbutton = document.querySelector("#emailbutton");
-var email = document.querySelector("#email", "input");
-
-function ToClipboard(){
-    navigator.clipboard.writeText(email.value)
-    
-}
