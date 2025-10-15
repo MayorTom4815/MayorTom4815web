4ee98e94ced88e127aa92521ce565fd9ec8066dd
MayorTom4815 <domiisac2004@proton.me>
2025-10-15 01:45:57 -0600

    Update 15Oct2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://mayortom4815.github.io",
-    base: "/MayorTom4815web"
+    base: "/MayorTom4815web/"
 });
