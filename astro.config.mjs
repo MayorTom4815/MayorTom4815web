e244b6e00129eb106e3513d83b16fabb9788b248
MayorTom4815 <domiisac2004@proton.me>
2025-11-02 23:32:30 -0600

    Update 2nov2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mayortom4815.github.io",
-    base: "/MayorTom4815web/"
+  site: "https://mayortom4815.github.io",
+  base: "/MayorTom4815web/",
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
